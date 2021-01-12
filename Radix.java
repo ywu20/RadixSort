385f1f03b6fdc0d0b47f812f727a894d3106b049
Yuqing Wu <ywu20@stuy.edu>
2021-01-12 13:13:27 -0500

    compilable merge

--- Radix.java
+++ Radix.java
@@ -4,12 +4,20 @@ public class Radix{
     return Integer.parseInt(nn.substring(col,col+1));
   }
 
+public static void merge(MyLinkedList original, MyLinkedList[] buckets){
+  for(int i=0;i<buckets.length;i++){
+    original.extend(buckets[i]);
+  }
+}
   public static int length(int n){
     String nn=Integer.toString(n);
     return nn.length();
   }
+
+
   public static void main (String[] args){
     System.out.println(nth(123, 1));
     System.out.println(length(123));
+    System.out.println();
   }
 }
