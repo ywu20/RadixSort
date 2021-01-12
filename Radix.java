4535a1287ffa769fd5f47e1226419440f2de10dc
Yuqing Wu <ywu20@stuy.edu>
2021-01-12 13:05:41 -0500

    nth

--- Radix.java
+++ Radix.java
@@ -1,5 +1,9 @@
 public class Radix{
+  public static int nth(int n, int col){
+    String nn=Integer.toString(n);
+    return Integer.parseInt(nn.substring(col,col+1));
+  }
   public static void main (String[] args){
-
+    System.out.println(nth(123, 1));
   }
 }
