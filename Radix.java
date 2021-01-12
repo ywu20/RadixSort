17ae4973ea85098ae83caf85c67b530c8fb174e9
Yuqing Wu <ywu20@stuy.edu>
2021-01-12 13:06:53 -0500

    length

--- Radix.java
+++ Radix.java
@@ -3,7 +3,13 @@ public class Radix{
     String nn=Integer.toString(n);
     return Integer.parseInt(nn.substring(col,col+1));
   }
+
+  public static int length(int n){
+    String nn=Integer.toString(n);
+    return nn.length();
+  }
   public static void main (String[] args){
     System.out.println(nth(123, 1));
+    System.out.println(length(123));
   }
 }
