3a1898985c36348730337beef9b39563182d34e4
Yuqing Wu <ywu20@stuy.edu>
2021-01-12 21:41:09 -0500

    simple tests

--- Radix.java
+++ Radix.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Radix{
   public static int nth(int n, int col){
     String nn=Integer.toString(n);
@@ -16,8 +17,27 @@ public static void merge(MyLinkedList original, MyLinkedList[] buckets){
 
 
   public static void main (String[] args){
+
+    /*
     System.out.println(nth(123, 1));
     System.out.println(length(123));
-    System.out.println();
+    MyLinkedList original=new MyLinkedList();
+    MyLinkedList[] buckets=new MyLinkedList[10];
+    for(int i=0;i<10;i++){
+      MyLinkedList inBucket=new MyLinkedList();
+      for(int j=0;j<3;j++){
+        inBucket.add(Integer.toString(i*10+j));
+      }
+      buckets[i]=inBucket;
+    }
+
+merge(original,buckets);
+merge(original, buckets);
+for(int i=0;i<10;i++){
+  buckets[i].add(Integer.toString(i*10+5));
+}
+merge(original, buckets);
+    System.out.println(original);
+  */
   }
 }
